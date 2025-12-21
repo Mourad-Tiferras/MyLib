16254ada24cf1af14f5fc1cbc38ef608657d59cd
ph4n70m88 <m.tiferras@gmail.com>
2025-12-21 17:33:29 +0100

    will be continued

--- mylib.hpp
+++ mylib.hpp
@@ -1,13 +1,13 @@
 #ifndef MYLIB_HPP
 #define MYLIB_HPP
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <string>
+
+# include <stdlib.h>
+# include <string.h>
+# include <string>
 
 
-char    *_itoa(int   num);
+char        *_itoa(int   num);
 std::string _substr(const std::string &str, size_t start, size_t len);
 std::string _strtrim(const std::string& str, const std::string& set);
 
