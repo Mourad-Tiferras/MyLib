bab6317ba8eb44ec45ae7ec2de39e6456acc33d5
ph4n70m88 <m.tiferras@gmail.com>
2025-12-26 13:22:42 +0100

    will be continued

--- mylib.hpp
+++ mylib.hpp
@@ -5,15 +5,14 @@
 # include <stdlib.h>
 # include <string.h>
 # include <string>
-# include <stdio.h>
 
 
-char        *_itoa(int   num);
-std::string _substr(const std::string &str, size_t start, size_t len);
-std::string _strtrim(std::string &str, std::string &set);
-std::string _strjoin(const std::string &s1, const std::string &s2);
-char        **ft_split(char const *s, char c);
-std::string _ROT13(const std::string &str);
+char            *_itoa(int   num);
+std::string     _substr(const std::string &str, size_t start, size_t len);
+std::string     _strtrim(std::string &str, std::string &set);
+std::string     _strjoin(const std::string &s1, const std::string &s2);
+char            **ft_split(char const *s, char c);
+std::string     _ROT13(const std::string &str);
 
 
 #endif
